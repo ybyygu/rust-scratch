0d2f2279f0ec5e5da7928e17f84437307c152270
Wenping Guo <ybyygu@gmail.com>
2017-12-10 20:38:57 +0800

    rust hello world

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+// [[file:~/Workspace/Programming/rust-scratch/rust.note::b08803c2-e9b1-4542-9574-b8c467d527b1][b08803c2-e9b1-4542-9574-b8c467d527b1]]
+fn main() {
+    println!("this is a hello {}", "world");
+    println!("{}", 12);
+}
+// b08803c2-e9b1-4542-9574-b8c467d527b1 ends here
