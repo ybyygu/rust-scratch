81be92c873873551f3987ded7e7f25ac836738fc
Wenping Guo <ybyygu@gmail.com>
2018-02-02 15:10:20 +0800

    read lammps data file

--- reaction/src/main.rs
+++ reaction/src/main.rs
@@ -126,7 +126,129 @@ fn test_formula() {
 }
 // 27fa4abe-d98c-4dd4-8695-e4b4f807cabc ends here
 
-// [[file:~/Workspace/Programming/rust-scratch/rust.note::b8ea57f0-b549-4fa0-ac1a-abf83009009e][b8ea57f0-b549-4fa0-ac1a-abf83009009e]]
+// [[file:~/Workspace/Programming/rust-scratch/rust.note::1f4bb42e-6c9c-41d1-b9f3-e0908813187a][1f4bb42e-6c9c-41d1-b9f3-e0908813187a]]
+fn parse_lammps_data_file(file: File) -> Result<String, io::Error>{
+    let mut reader = BufReader::new(file);
+    let mut lines_iter = reader.lines();
+
+    // skip the first two lines
+    for _ in 0..2 {
+        lines_iter.next();
+    }
+    // 1. read number of total atoms
+    // 684  atoms
+    let mut natoms = 0;
+    let nresult = lines_iter.next();
+    println!("{:?}", nresult);
+    if let Some(line) = nresult {
+        // pick out plain string, propagate IO error if any
+        let line = line?;
+        assert!(line.contains(" atoms"), format!("cannot find number of atoms: {}", line));
+        let v: Vec<_> = line.split_whitespace().collect();
+        natoms = v[0].parse().unwrap();
+    } else {
+        eprintln!("{:?}", nresult);
+    }
+
+    // 2. read in number of atom types
+    let mut natom_types = 0_usize;
+    loop {
+        if let Some(line) = lines_iter.next() {
+            let line = line?;
+            println!("{:?}", line);
+            if line.ends_with("atom types") {
+                if let Some(n) = line.split_whitespace().nth(0) {
+                    natom_types = n.parse().unwrap();
+                }
+                break;
+            }
+        } else {
+            panic!("cannot find atom types lines in lammps data file");
+        }
+    }
+
+    // 3. parse atom types
+    // 3.0 jump to Masses section
+    loop {
+        if let Some(line) = lines_iter.next() {
+            let line = line?;
+            if line.starts_with("Masses") {
+                break;
+            }
+        }
+    }
+    // 3.1 read in atom type maping
+    // NOTE: element symbol is supposed to be after `#`
+    //     1  50.941500   # V
+    // skip one blank line
+    assert!(natom_types > 0);
+    lines_iter.next();
+
+    // mapping: atom_index ==> atom_symbol
+    let mut mapping_symbols = HashMap::new();
+    for _ in 0..natom_types {
+        if let Some(line) = lines_iter.next() {
+            let line = line.unwrap();
+            let mut attrs = line.split_whitespace();
+            let k = attrs.nth(0).unwrap();
+            let v = attrs.last().unwrap();
+            mapping_symbols.insert(k.to_string(), v.to_string());
+        }
+    }
+    println!("{:?}", mapping_symbols);
+
+    // 3. jump to Atom section
+    loop {
+        if let Some(line) = lines_iter.next() {
+            let line = line?;
+            if line.starts_with("Atom") {
+                break;
+            }
+        } else {
+            panic!("cannot find Atom lines in lammps data file");
+        }
+    }
+    // skip one blank line
+    lines_iter.next();
+    // 4. read in atom index and atom type
+    assert!(natoms > 0);
+    for _ in 0..natoms {
+        if let Some(line) = lines_iter.next() {
+            let line = line?;
+            // println!("{}", line);
+        } else {
+            panic!("Atom records are incomplete.");
+        }
+    }
+
+    Ok("G".to_string())
+}
+
+fn open_file(filename: &str) -> Result<String, io::Error> {
+    // 1. guess required lammps files from input filename
+    let path = Path::new(filename);
+    let path_lammps_data = path.with_extension("data");
+    let path_lammps_bonds = path.with_extension("bonds");
+    let path_lammps_dump = path.with_extension("dump");
+
+    // assert!(path_lammps_data.is_file());
+    // assert!(path_lammps_bonds.is_file());
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let f = File::open(path_lammps_data)?;
+    parse_lammps_data_file(f);
+
+    Ok("Good".to_string())
+}
+
+#[test]
+fn test_open_file() {
+    let path = "/home/ybyygu/Workspace/Projects/structure-prediction/data/e2/789648-d084-401b-a67e-e9628a29ca12/测试文件/V2O5_010_MeOH_rand_nvt_650_20.bonds";
+    let fp = open_file(path);
+}
+// 1f4bb42e-6c9c-41d1-b9f3-e0908813187a ends here
+
+// [[file:~/Workspace/Programming/rust-scratch/rust.note::84783441-0f98-4bd5-87a2-44b54dac4b22][84783441-0f98-4bd5-87a2-44b54dac4b22]]
 fn get_edge_from_line(line: String) -> (String, String, Vec<(String, String)>) {
     //  301 4 3 289 308 307 0         1.129         1.232         1.231         3.591         0.083         0.362
     let line = &*line;
@@ -273,7 +395,9 @@ fn read_from_file(filename: &str){
         lines_iter.next();
     }
 }
+// 84783441-0f98-4bd5-87a2-44b54dac4b22 ends here
 
+// [[file:~/Workspace/Programming/rust-scratch/rust.note::b8ea57f0-b549-4fa0-ac1a-abf83009009e][b8ea57f0-b549-4fa0-ac1a-abf83009009e]]
 /// get file name from command line argument
 fn get_filename() -> Result<String, String> {
     use clap::{App, Arg, AppSettings};
@@ -307,6 +431,8 @@ fn get_filename() -> Result<String, String> {
 
 fn main() {
     let filename = get_filename().unwrap();
-    read_from_file(filename.as_str());
+    // read_from_file(filename.as_str());
+    let r = open_file(filename.as_str());
+    println!("{:?}", r);
 }
 // b8ea57f0-b549-4fa0-ac1a-abf83009009e ends here
